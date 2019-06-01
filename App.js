b01d70fdfc48b2d245c4550992d0769dea739a21
Artem K <artem.k.here@gmail.com>
2019-06-01 10:05:46 -0700

    Add Animated example as a boilerplate

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import BreathingSlide from './breathingSlide';
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Testing with Jest-Expo!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <BreathingSlide />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ breathingSlide.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {
+    Animated, TouchableWithoutFeedback, Text, View, StyleSheet
+} from 'react-native';
+
+export default class BreathingSlide extends Component {
+    constructor(props) {
+        super(props);
+
+        this.moveAnimation = new Animated.ValueXY({ x: 10, y: 450 });
+    }
+
+    _moveBall = () => {
+        Animated.spring(this.moveAnimation, {
+            toValue: {x: 250, y: 10},
+        }).start();
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Animated.View style={[styles.tennisBall, this.moveAnimation.getLayout()]}>
+                    <TouchableWithoutFeedback style={styles.button} onPress={this._moveBall}>
+                        <Text style={styles.buttonText}>Press</Text>
+                    </TouchableWithoutFeedback>
+                </Animated.View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ecf0f1',
+    },
+    tennisBall: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'greenyellow',
+        borderRadius: 100,
+        width: 100,
+        height: 100,
+    },
+    button: {
+        paddingTop: 24,
+        paddingBottom: 24,
+    },
+    buttonText: {
+        fontSize: 24,
+        color: '#333',
+    }
+});
