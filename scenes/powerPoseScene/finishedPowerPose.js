ad445d7c8ed95631127e091b017a15cab1ef0593
Artem K <artem.k.here@gmail.com>
2019-06-11 21:12:08 -0700

    First Pass

--- scenes/powerPoseScene/finishedPowerPose.js
+++ scenes/powerPoseScene/finishedPowerPose.js
@@ -4,9 +4,9 @@ import { Text, View } from 'react-native';
 export default class FinishedPowerPose extends Component {
     render() {
         return (
-            <View}>
-                <Text>All Done</Text>
             <View>
+                <Text>All Done</Text>
+            </View>
         );
     }
 }
--- scenes/powerPoseScene/playingPowerPose.js
+++ scenes/powerPoseScene/playingPowerPose.js
@@ -10,6 +10,7 @@ export default class PlayingBreathing extends Component {
 
         this.state = {
             progressCircleCompletion: new Animated.Value(0),
+            progressCircleCompletionValue: 0
         }
     }
 
@@ -23,34 +24,42 @@ export default class PlayingBreathing extends Component {
 
         const progressCircleCompletion = this.state.progressCircleCompletion;
 
+        progressCircleCompletion.addListener(({ value }) => {
+            this.setState({ progressCircleCompletionValue: Math.ceil(value) });
+        });
+
         Animated.timing(progressCircleCompletion, {
             toValue: 100,
-            duration: sceneScenario[0] * 1000,
+            duration: sceneScenario[0] * 1000
         }).start((event) => {
             if (event.finished) { handleSceneEnd(); }
         })
     }
 
     render() {
-        const progressCircleCompletion = this.state.progressCircleCompletion;
+        const {
+            progressCircleCompletion,
+            progressCircleCompletionValue
+        } = this.state;
 
         return (
-            <Animated.View>
+            <View>
                 <ProgressCircle
-                    percent={progressCircleCompletion}
+                    percent={progressCircleCompletionValue}
                     radius={50}
                     borderWidth={8}
                     color="#3399FF"
                     shadowColor="#999"
                     bgColor="#fff"
                 >
-                    <Text style={{ fontSize: 18 }}>{progressCircleCompletion}%</Text>
+                    <Text style={{ fontSize: 18 }}>Power Pose Image</Text>
                 </ProgressCircle>
                 <Text>Strike a Power Pose</Text>
-            </Animated.View>
+            </View>
         );
     }
 }
+
 
 PlayingBreathing.propTypes = {
     sceneScenario: PropTypes.array,
